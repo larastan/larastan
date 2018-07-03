db7a5dfd2e6b611aaeb5cff1a8c5d922d9dded01
Nuno Maduro <enunomaduro@gmail.com>
2018-07-03 23:37:18 +0200

    feat: bootstraps laravel app

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of Laravel Code Analyse.
+ *
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+define('LARAVEL_START', microtime(true));
+
+$app = require_once __DIR__ . '/../../../bootstrap/app.php';
+
+$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
