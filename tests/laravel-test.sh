2e559019945feaeadfb3c26092dba7c9b88d0c1b
Viktor Szépe <viktor@szepe.net>
2018-07-20 00:25:13 +0000

    Use the commit that the current build is testing

--- tests/laravel-test.sh
+++ tests/laravel-test.sh
@@ -8,7 +8,7 @@ cd laravel/
 
 # Add package from source
 sed -e 's|"type": "project",|&\n"repositories": [ { "type": "vcs", "url": "../" } ],|' -i composer.json
-composer require --dev nunomaduro/larastan
+composer require --dev "nunomaduro/larastan:dev-master#${TRAVIS_COMMIT}"
 
 # Fix https://github.com/laravel/framework/pull/23825
 sed -e 's|@return \\Illuminate\\Http\\Response$|@return \\Symfony\\Component\\HttpFoundation\\Response|' \
