9da647615edb8562de86643834d634e4376b9faf
Julio Foulquie <jfoulquie@gmail.com>
2018-09-25 17:59:09 -0300

    Change copyright to package owner

--- src/ReturnTypes/Helpers/RequestExtension.php
+++ src/ReturnTypes/Helpers/RequestExtension.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of Larastan.
  *
- * (c) Julio Foulquie <jfoulquie@gmail.com>
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
  *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
