11671b5fd41a2c05a31c738c5e26cb5c6522f9f5
Viktor Szépe <viktor@szepe.net>
2019-02-15 13:36:19 +0100

    Lumen testing needs registration

--- tests/laravel-test.sh
+++ tests/laravel-test.sh
@@ -29,5 +29,28 @@ echo "Add package from source"
 sed -e 's|"type": "project",|&\n"repositories": [ { "type": "vcs", "url": "../" } ],|' -i composer.json
 travis_retry composer require --dev "nunomaduro/larastan:dev-master#${TRAVIS_COMMIT}"
 
+echo "Add Larastan to Lumen"
+cat <<"EOF" | patch
+--- bootstrap/app.php     2019-02-15 12:31:48.469773495 +0000
++++ bootstrap/app.php     2019-02-15 12:27:43.358369317 +0000
+@@ -23,6 +23,8 @@
+     dirname(__DIR__)
+ );
+
++$app->instance('path.storage', app()->basePath() . DIRECTORY_SEPARATOR . 'storage');
++
+ // $app->withFacades();
+
+ // $app->withEloquent();
+@@ -78,6 +80,7 @@
+ |
+ */
+
++$app->register(NunoMaduro\Larastan\LarastanServiceProvider::class);
+ // $app->register(App\Providers\AppServiceProvider::class);
+ // $app->register(App\Providers\AuthServiceProvider::class);
+ // $app->register(App\Providers\EventServiceProvider::class);
+EOF
+
 echo "Test Lumen"
 php artisan code:analyse --level=max
