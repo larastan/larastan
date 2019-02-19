f01cda7569d1591540c060cfe01a7bf4c54185a8
Claystation <c.wijnekus@gmail.com>
2019-02-19 10:58:40 +0100

    Added test and extension for file method on Request.

---
+++ tests/Features/ReturnTypes/RequestFileExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features\ReturnTypes;
+
+use Illuminate\Http\Request;
+
+class RequestFileExtension
+{
+
+    public function testRequestFileStore()
+    {
+        $request = new Request();
+        $request->file('image')->store('/path');
+    }
+}
