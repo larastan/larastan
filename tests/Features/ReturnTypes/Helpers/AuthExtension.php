ee36284c42f265bfed853c854d896e72bf4b2dfe
Uksusov Kirill <uksusoff@yandex.ru>
2019-03-07 17:51:04 +0400

    CS fixed

--- tests/Features/ReturnTypes/Helpers/AuthExtension.php
+++ tests/Features/ReturnTypes/Helpers/AuthExtension.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Features\ReturnTypes\Helpers;
 
-use Illuminate\Contracts\Auth\Factory;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Factory;
 
 class AuthExtension
 {
