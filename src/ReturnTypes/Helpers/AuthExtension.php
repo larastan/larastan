f893cba6e2309e410581c82cca484ffe9e5189ab
Uksusov Kirill <uksusoff@yandex.ru>
2019-03-07 17:04:53 +0400

    Fixed https://github.com/nunomaduro/larastan/issues/44

---
+++ src/ReturnTypes/Helpers/AuthExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Larastan.
+ *
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace NunoMaduro\Larastan\ReturnTypes\Helpers;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+/**
+ * @internal
+ */
+final class AuthExtension implements DynamicFunctionReturnTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isFunctionSupported(FunctionReflection $functionReflection): bool
+    {
+        return $functionReflection->getName() === 'auth';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTypeFromFunctionCall(
+        FunctionReflection $functionReflection,
+        FuncCall $functionCall,
+        Scope $scope
+    ): Type {
+        if (isset($functionCall->args[0]->value) && $functionCall->args[0]->value === null) {
+            return new ObjectType(\Illuminate\Contracts\Auth\Factory::class);
+        }
+
+        return new ObjectType(\Illuminate\Contracts\Auth\Guard::class);
+    }
+}
