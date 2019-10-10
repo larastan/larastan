ec67a4d6e0533d1bf9d845418f3437877610b528
Can Vural <can9119@gmail.com>
2019-10-10 13:50:28 +0200

    Add missing license

--- src/ReturnTypes/EloquentBuilderExtension.php
+++ src/ReturnTypes/EloquentBuilderExtension.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of Larastan.
+ *
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 namespace NunoMaduro\Larastan\ReturnTypes;
 
 use PHPStan\Type\Type;
