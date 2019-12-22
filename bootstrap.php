c3ad04eab48c2a43aa7a4e616272664549fc0370
Can Vural <can9119@gmail.com>
2019-12-22 23:07:25 +0100

    Fix path for ApplicationResolver

--- bootstrap.php
+++ bootstrap.php
@@ -15,9 +15,7 @@ use NunoMaduro\Larastan\ApplicationResolver;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($applicationPath = __DIR__.'/../../bootstrap/app.php')) { // Applications
-    $app = require $applicationPath;
-} elseif (file_exists($applicationPath = getcwd().'/../../bootstrap/app.php')) { // Local Dev
+if (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Applications and Local Dev
     $app = require $applicationPath;
 } else { // Packages
     $app = ApplicationResolver::resolve();
