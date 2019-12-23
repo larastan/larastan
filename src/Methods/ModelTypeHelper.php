e924cfe82b4ce7cf2e0b50d117331564534e7be6
Can Vural <can9119@gmail.com>
2019-12-23 17:58:53 +0100

    Fixes replacing type logic in ModelTypeHelper

--- src/Methods/ModelTypeHelper.php
+++ src/Methods/ModelTypeHelper.php
@@ -14,14 +14,12 @@ declare(strict_types=1);
 namespace NunoMaduro\Larastan\Methods;
 
 use Illuminate\Database\Eloquent\Model;
-use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\IntersectionType;
 use PHPStan\Type\IterableType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\ObjectWithoutClassType;
 use PHPStan\Type\StaticType;
 use PHPStan\Type\Type;
-use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\TypeTraverser;
 use PHPStan\Type\UnionType;
 
@@ -29,22 +27,24 @@ final class ModelTypeHelper
 {
     public static function replaceStaticTypeWithModel(Type $type, string $modelClass) : Type
     {
-        $type = TypeTraverser::map($type, static function (Type $type, callable $traverse) use ($modelClass): Type {
+        return TypeTraverser::map($type, static function (Type $type, callable $traverse) use ($modelClass): Type {
             if ($type instanceof UnionType || $type instanceof IntersectionType) {
                 return $traverse($type);
             }
 
-            if ($type instanceof IterableType) {
-                return $traverse($type->getItemType());
+            if ($type instanceof IterableType && $type->getItemType() instanceof StaticType) {
+                return new IterableType($type->getIterableKeyType(), new ObjectType($modelClass));
             }
 
             if ($type instanceof ObjectWithoutClassType || $type instanceof StaticType) {
-                return new GenericObjectType(Model::class, [new ObjectType($modelClass)]);
+                return new ObjectType($modelClass);
+            }
+
+            if ($type instanceof ObjectType && $type->getClassName() === Model::class) {
+                return new ObjectType($modelClass);
             }
 
             return $type;
         });
-
-        return TypeCombinator::remove($type, new ObjectType(Model::class, new ObjectType($modelClass)));
     }
 }
