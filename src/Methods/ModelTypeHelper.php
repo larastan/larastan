86efc0d1c1015f9da45e3ecba7dd53657df9025f
Can Vural <can9119@gmail.com>
2019-12-23 23:36:22 +0100

    Simplify replacing type logic in ModelTypeHelper

--- src/Methods/ModelTypeHelper.php
+++ src/Methods/ModelTypeHelper.php
@@ -14,37 +14,27 @@ declare(strict_types=1);
 namespace NunoMaduro\Larastan\Methods;
 
 use Illuminate\Database\Eloquent\Model;
-use PHPStan\Type\IntersectionType;
-use PHPStan\Type\IterableType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\ObjectWithoutClassType;
 use PHPStan\Type\StaticType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeTraverser;
-use PHPStan\Type\UnionType;
+use PHPStan\Type\TypeWithClassName;
 
 final class ModelTypeHelper
 {
     public static function replaceStaticTypeWithModel(Type $type, string $modelClass) : Type
     {
         return TypeTraverser::map($type, static function (Type $type, callable $traverse) use ($modelClass): Type {
-            if ($type instanceof UnionType || $type instanceof IntersectionType) {
-                return $traverse($type);
-            }
-
-            if ($type instanceof IterableType && $type->getItemType() instanceof StaticType) {
-                return new IterableType($type->getIterableKeyType(), new ObjectType($modelClass));
-            }
-
             if ($type instanceof ObjectWithoutClassType || $type instanceof StaticType) {
                 return new ObjectType($modelClass);
             }
 
-            if ($type instanceof ObjectType && $type->getClassName() === Model::class) {
+            if ($type instanceof TypeWithClassName && $type->getClassName() === Model::class) {
                 return new ObjectType($modelClass);
             }
 
-            return $type;
+            return $traverse($type);
         });
     }
 }
