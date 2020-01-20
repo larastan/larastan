6df33359898f959d0351fd20f88588e618b3f99e
Can Vural <can9119@gmail.com>
2020-01-20 12:27:09 +0100

    Add missing licenses

--- src/Types/ModelRelationsDynamicMethodReturnTypeExtension.php
+++ src/Types/ModelRelationsDynamicMethodReturnTypeExtension.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of Larastan.
+ *
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 namespace NunoMaduro\Larastan\Types;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
