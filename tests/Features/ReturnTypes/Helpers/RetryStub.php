afd30e838ff87e9cfe76b50137822eec95337cb5
Daan <daanraatjes@gmail.com>
2020-04-23 16:19:53 +0200

    Add retry helper stub

---
+++ tests/Features/ReturnTypes/Helpers/RetryStub.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features\ReturnTypes\Helpers;
+
+use App\User;
+
+class RetryStub
+{
+    public function testRetryVoid(): void
+    {
+        retry(1, function () {
+        });
+    }
+
+    public function testRetryInt(): int
+    {
+        return retry(5, function (): int {
+            return 5;
+        });
+    }
+
+    public function testRetryWhen(): ?User
+    {
+        return retry(5, function (): ?User {
+            return User::first();
+        }, 0, function (): bool {
+            return true;
+        });
+    }
+}
