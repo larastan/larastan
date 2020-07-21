b5f96b4df2d0931a6eb00a5c6539c56df2d67bd9
Can Vural <can9119@gmail.com>
2020-07-21 22:05:42 +0200

    fix: dont override return type when eloquent builder returns a query builder instance

--- tests/Features/Methods/Builder.php
+++ tests/Features/Methods/Builder.php
@@ -6,6 +6,8 @@ namespace Tests\Features\Methods;
 
 use App\User;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use stdClass;
 
 class Builder
 {
@@ -27,5 +29,31 @@ class Builder
     public function testDynamicWhereAsInt(): ?User
     {
         return (new User())->whereFoo(1)->first();
+    }
+
+    public function testGetQueryReturnsQueryBuilder(): QueryBuilder
+    {
+        return User::getQuery();
+    }
+
+    public function testToBaseReturnsQueryBuilder(): QueryBuilder
+    {
+        return User::toBase();
+    }
+
+    public function testToBaseReturnsQueryBuilderAfterChain(): QueryBuilder
+    {
+        return User::whereNull('foo')
+            ->orderBy('fooBar')
+            ->toBase();
+    }
+
+    public function testQueryBuilderChainStartedWithGetQueryReturnsStdClass(): ?stdClass
+    {
+        return User::getQuery()
+        ->select('some_model.created')
+        ->where('some_model.some_column', '=', true)
+        ->orderBy('some_model.created', 'desc')
+        ->first();
     }
 }
