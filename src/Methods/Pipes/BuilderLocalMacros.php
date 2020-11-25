55b4d797166b35cec42d5dc4a9b0dcdc0a4a59e7
Can Vural <can9119@gmail.com>
2020-11-25 23:50:22 +0100

    fix: use function map instead of calling newQuery on model (#717)

--- src/Methods/Pipes/BuilderLocalMacros.php
+++ src/Methods/Pipes/BuilderLocalMacros.php
@@ -8,7 +8,6 @@ use Closure;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use function in_array;
 use NunoMaduro\Larastan\Concerns;
 use NunoMaduro\Larastan\Contracts\Methods\PassableContract;
@@ -18,7 +17,6 @@ use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\Generic\TemplateTypeMap;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\TypehintHelper;
-use ReflectionClass;
 use ReflectionFunction;
 use ReflectionParameter;
 
@@ -42,18 +40,19 @@ final class BuilderLocalMacros implements PipeContract
 
         if ($classReflection->isSubclassOf(Model::class) && in_array(SoftDeletes::class,
                 trait_uses_recursive($classReflection->getName()), true)) {
-            $model = new class extends Model {
-            };
+            $methods = [
+                'restore' => function (Builder $builder) {
+                },
+                'withTrashed' => function (Builder $builder, bool $withTrashed = true) {
+                },
+                'withoutTrashed' => function (Builder $builder) {
+                },
+                'onlyTrashed' => function (Builder $builder) {
+                },
+            ];
 
-            (new SoftDeletingScope)->extend($builder = $model->newQuery());
-
-            $refObject = new ReflectionClass(Builder::class);
-            $refProperty = $refObject->getProperty('localMacros');
-            $refProperty->setAccessible(true);
-            $localMacros = $refProperty->getValue($builder);
-
-            if (array_key_exists($passable->getMethodName(), $localMacros)) {
-                $reflectionFunction = new ReflectionFunction($localMacros[$passable->getMethodName()]);
+            if (array_key_exists($passable->getMethodName(), $methods)) {
+                $reflectionFunction = new ReflectionFunction($methods[$passable->getMethodName()]);
                 $parameters = $reflectionFunction->getParameters();
                 unset($parameters[0]); // The query argument.
                 $parameters = array_values($parameters);
