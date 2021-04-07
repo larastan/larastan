1e8b0aa41e9454b7c1dc584364ccd0373428afc3
Can Vural <can9119@gmail.com>
2021-04-07 21:50:20 +0200

    fix: PHPStan 0.12.82 compatibility

--- tests/Features/ReturnTypes/BuilderExtension.php
+++ tests/Features/ReturnTypes/BuilderExtension.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use function PHPStan\dumpType;
 
 class BuilderExtension
 {
