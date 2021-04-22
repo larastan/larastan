185f3d39f12aec095b3795014a6991e4a92312b5
Thibaud Dauce <git@hadibut.fr>
2021-04-22 13:30:16 +0200

    Add support for Laravel 8 and upper tests

---
+++ tests/Features/Laravel8/Methods/ModelExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features\Laravel8\Methods;
+
+class ModelExtension
+{
+    /**
+     * This test can be remove once we add real tests for Laravel 8 features
+     * 
+     * @throws \Illuminate\Database\RecordsNotFoundException
+     */
+    public function testCanThrowLaravel8Exception(): void
+    {
+
+    }
+}
