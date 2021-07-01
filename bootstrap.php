d8d1a901428acd28c5bafe37027d1e96a8d397ac
rajyan <38206553+rajyan@users.noreply.github.com>
2021-07-01 18:33:47 +0900

    fix bootstrap path (#852)

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ use NunoMaduro\Larastan\ApplicationResolver;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Applications and Local Dev
+if (file_exists($applicationPath = dirname(__DIR__, 3).'/bootstrap/app.php')) { // Applications and Local Dev
     $app = require $applicationPath;
 } else { // Packages
     $app = ApplicationResolver::resolve();
--- tests/laravel-test.sh
+++ tests/laravel-test.sh
@@ -7,7 +7,7 @@ composer create-project --quiet --prefer-dist "laravel/laravel" ../laravel
 cd ../laravel/
 
 echo "Add package from source"
-sed -e 's|"type": "project",|&\n"repositories": [ { "type": "path", "url": "../larastan" } ],|' -i composer.json
+sed -e 's|"type": "project",|&\n"repositories": [ { "type": "path", "url": "../larastan", "options": { "symlink": false }} ],|' -i composer.json
 composer require --dev "nunomaduro/larastan:*"
 
 echo "Fix https://github.com/laravel/framework/pull/23825"
@@ -22,12 +22,15 @@ echo "Test Laravel"
 vendor/bin/phpstan analyse app --level=5 -c vendor/nunomaduro/larastan/extension.neon
 cd -
 
+echo "Test Laravel from other working directories"
+../laravel/vendor/bin/phpstan analyse ../laravel/app --level=5 -c ../laravel/vendor/nunomaduro/larastan/extension.neon
+
 echo "Install Lumen"
 composer create-project --quiet --prefer-dist "laravel/lumen" ../lumen
 cd ../lumen/
 
 echo "Add package from source"
-sed -e 's|"type": "project",|&\n"repositories": [ { "type": "path", "url": "../larastan" } ],|' -i composer.json
+sed -e 's|"type": "project",|&\n"repositories": [ { "type": "path", "url": "../larastan", "options": { "symlink": false }} ],|' -i composer.json
 composer require --dev "nunomaduro/larastan:*"
 
 echo "Fix Handler::render return type"
@@ -60,3 +63,7 @@ EOF
 
 echo "Test Lumen"
 vendor/bin/phpstan analyse app --level=5 -c vendor/nunomaduro/larastan/extension.neon
+cd -
+
+echo "Test Lumen from other working directories"
+../lumen/vendor/bin/phpstan analyse ../lumen/app --level=5 -c ../lumen/vendor/nunomaduro/larastan/extension.neon
