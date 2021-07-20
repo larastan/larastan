52ee918dac519e68a3ead9e93a270f13ec92c07e
Benedikt Franke <benedikt.franke@mll.com>
2021-07-20 14:00:27 +0200

    Avoid need for explicit `@extends` in factories

--- tests/Laravel8Application/database/factories/UserFactory.php
+++ tests/Laravel8Application/database/factories/UserFactory.php
@@ -8,23 +8,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel8\Models\User;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
         return [
