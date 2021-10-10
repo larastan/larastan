3e28da4fbfd89e5be37f88031906035e2783e1c5
Yohta Kimura <kitakita7617@gmail.com>
2021-10-10 21:18:05 +0900

    handle all cases, without breaking backward compatibility

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,9 @@ use NunoMaduro\Larastan\ApplicationResolver;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Applications and Local Dev
+if (file_exists($applicationPath = dirname(__DIR__, 3).'/bootstrap/app.php')) { // Relative path from default Laravel vendor dir
+    $app = require $applicationPath;
+} elseif (file_exists($applicationPath =  getcwd().'/bootstrap/app.php')) { // Handle cases where vendor is not installed in default path. see https://github.com/nunomaduro/larastan/issues/856
     $app = require $applicationPath;
 } elseif (trait_exists(Orchestra\Testbench\Concerns\CreatesApplication::class)) { // Packages
     $app = ApplicationResolver::resolve();
