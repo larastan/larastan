ef8e81c7d042dc59f1830ea19bbe11c46bbb0e00
Yohta Kimura <kitakita7617@gmail.com>
2021-10-10 21:38:31 +0900

    fix styles

--- bootstrap.php
+++ bootstrap.php
@@ -8,12 +8,12 @@ define('LARAVEL_START', microtime(true));
 
 if (file_exists($applicationPath = dirname(__DIR__, 3).'/bootstrap/app.php')) { // Relative path from default Laravel vendor dir
     $app = require $applicationPath;
-} elseif (file_exists($applicationPath =  getcwd().'/bootstrap/app.php')) { // Handle cases where vendor is not installed in default path. see https://github.com/nunomaduro/larastan/issues/856
+} elseif (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Handle cases where vendor is not installed in default path. see https://github.com/nunomaduro/larastan/issues/856
     $app = require $applicationPath;
 } elseif (trait_exists(Orchestra\Testbench\Concerns\CreatesApplication::class)) { // Packages
     $app = ApplicationResolver::resolve();
 } else {
-    throw new \Exception("Could not find Laravel bootstrap file nor Testbench is installed. Install orchestra/testbench if using larastan for Laravel packages.");
+    throw new \Exception('Could not find Laravel bootstrap file nor Testbench is installed. Install orchestra/testbench if using larastan for Laravel packages.');
 }
 
 if ($app instanceof \Illuminate\Contracts\Foundation\Application) {
