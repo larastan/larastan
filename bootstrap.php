3deba5342fbbe0efa65fad0d67b464fe73f1bfef
Yohta Kimura <kitakita7617@gmail.com>
2021-10-10 21:04:22 +0900

    throw an Exception if bootstrap.php nor orchestra/testbench doesn't exist

--- bootstrap.php
+++ bootstrap.php
@@ -6,11 +6,12 @@ use NunoMaduro\Larastan\ApplicationResolver;
 
 define('LARAVEL_START', microtime(true));
 
-$app = null;
 if (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Applications and Local Dev
     $app = require $applicationPath;
 } elseif (trait_exists(Orchestra\Testbench\Concerns\CreatesApplication::class)) { // Packages
     $app = ApplicationResolver::resolve();
+} else {
+    throw new \Exception("Could not find Laravel bootstrap file nor Testbench is installed. Install orchestra/testbench if using larastan for Laravel packages.");
 }
 
 if ($app instanceof \Illuminate\Contracts\Foundation\Application) {
