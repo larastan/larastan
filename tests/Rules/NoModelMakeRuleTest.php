e999c6655e2ed06f85f938514e5bd71136f12df8
Benedikt Franke <benedikt.franke@mll.com>
2021-10-22 13:23:20 +0200

    Fix line number

--- tests/Rules/NoModelMakeRuleTest.php
+++ tests/Rules/NoModelMakeRuleTest.php
@@ -20,7 +20,7 @@ class NoModelMakeRuleTest extends RulesTest
 
         self::assertEquals([
             13 => "Called 'Model::make()' which performs unnecessary work, use 'new Model()'.",
-            19 => "Called 'Model::make()' which performs unnecessary work, use 'new Model()'.",
+            20 => "Called 'Model::make()' which performs unnecessary work, use 'new Model()'.",
         ], $errors);
     }
 }
