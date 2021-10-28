fd6a51f7ca31253cd593434b9766ca7ae184c00b
Can Vural <can9119@gmail.com>
2021-10-28 15:27:26 +0200

    refactor: move collection helper test to new type inference test

---
+++ tests/Type/CollectionDynamicReturnTypeExtensionsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Type;
+
+class CollectionDynamicReturnTypeExtensionsTest extends \PHPStan\Testing\TypeInferenceTestCase
+{
+    /**
+     * @return iterable<mixed>
+     */
+    public function dataFileAsserts(): iterable
+    {
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/collection-helper.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     */
+    public function testFileAsserts(
+        string $assertType,
+        string $file,
+        ...$args
+    ): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/../../extension.neon'];
+    }
+}
---
+++ tests/Type/data/collection-helper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CollectionHelper;
+
+use App\User;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
+use function PHPStan\Testing\assertType;
+
+assertType('Illuminate\Support\Collection', collect());
+assertType('Illuminate\Support\Collection<0, 1>', collect(1));
+assertType('Illuminate\Support\Collection<0, \'foo\'>', collect('foo'));
+assertType('Illuminate\Support\Collection<0, 3.14>', collect(3.14));
+assertType('Illuminate\Support\Collection<0, true>', collect(true));
+assertType('Illuminate\Support\Collection<int|string, mixed>', collect([]));
+assertType('Illuminate\Support\Collection<int, int>', collect([1, 2, 3]));
+assertType('Illuminate\Support\Collection<int, string>', collect(['foo', 'bar', 'baz']));
+assertType('Illuminate\Support\Collection<int, float>', collect([1.0, 2.0, 3.0]));
+assertType('Illuminate\Support\Collection<int, float|int|string>', collect([1, 'foo', 1.0]));
+
+/**  @phpstan-param EloquentCollection<int> $eloquentCollection */
+function eloquentCollectionInteger(EloquentCollection $eloquentCollection): void
+{
+    assertType('Illuminate\Support\Collection<int, int>', collect($eloquentCollection));
+}
+
+/**  @phpstan-param EloquentCollection<User> $eloquentCollection */
+function eloquentCollectionUser(EloquentCollection $eloquentCollection): void
+{
+    assertType('Illuminate\Support\Collection<int, App\User>', collect($eloquentCollection));
+}
+
+
+/**
+ * @phpstan-param \Traversable<int, int> $foo
+ * @phpstan-return Collection<int, int>
+ */
+function testIterable(\Traversable $foo): void
+{
+    assertType('Illuminate\Support\Collection<int, int>', collect($foo));
+}
