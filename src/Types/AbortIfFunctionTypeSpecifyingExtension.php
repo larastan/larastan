2b8372764bbc8f5d9b5bc3e9b1e7f5529310f33d
Can Vural <can9119@gmail.com>
2021-11-01 12:00:31 +0100

    refactor: removes license header

--- src/Types/AbortIfFunctionTypeSpecifyingExtension.php
+++ src/Types/AbortIfFunctionTypeSpecifyingExtension.php
@@ -4,15 +4,6 @@ declare(strict_types=1);
 
 namespace NunoMaduro\Larastan\Types;
 
-/*
- * This file is part of Larastan.
- *
- * (c) Nuno Maduro <enunomaduro@gmail.com>
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- */
-
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
