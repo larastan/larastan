a4e8687731b4ecec09203e8e0021f28954aa1c44
Matan Yadaev <matan.yed@gmail.com>
2021-11-25 20:36:05 +0200

    add test

--- tests/Features/Methods/CarbonExtension.php
+++ tests/Features/Methods/CarbonExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Features\Methods;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Date;
 
 Carbon::macro('foo', static function (): string {
     return 'foo';
@@ -20,5 +21,20 @@ class CarbonExtension
     public function testCarbonMacroCalledDynamically(): string
     {
         return Carbon::now()->foo();
+    }
+
+    public function testCarbonMacroCallFromNowHelper(): string
+    {
+        return now()->foo();
+    }
+
+    public function testCarbonMacroCallFromDateFacade(): string
+    {
+        return Date::now()->foo();
+    }
+
+    public function testCarbonMacroCallFromSupportCarbonFacade(): string
+    {
+        return \Illuminate\Support\Carbon::now()->foo();
     }
 }
