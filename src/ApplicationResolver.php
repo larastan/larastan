ce5633ac4763c92e1e8e7dda44e25bf83af8ddc9
Josh Bonfield <josh@grizzlyware.com>
2022-01-19 20:01:03 +0000

    chore: support non-typed props

--- src/ApplicationResolver.php
+++ src/ApplicationResolver.php
@@ -20,7 +20,8 @@ final class ApplicationResolver
     /** @var mixed */
     public static $composer;
     
-    protected bool $enablesPackageDiscoveries = true;
+    /** $var bool */
+    protected $enablesPackageDiscoveries = true;
 
     /**
      * Creates an application and registers service providers found.
