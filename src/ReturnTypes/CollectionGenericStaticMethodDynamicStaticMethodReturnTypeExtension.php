708fef51a3a902d9479a968e9795b7d0b80793fb
Can Vural <can9119@gmail.com>
2022-01-20 22:08:22 +0100

    feat: add extensions to correct return types of some collection methods

---
+++ src/ReturnTypes/CollectionGenericStaticMethodDynamicStaticMethodReturnTypeExtension.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace NunoMaduro\Larastan\ReturnTypes;
+
+use Illuminate\Support\Collection;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
+use PHPStan\Type\ErrorType;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeTraverser;
+use PHPStan\Type\UnionType;
+
+class CollectionGenericStaticMethodDynamicStaticMethodReturnTypeExtension implements DynamicStaticMethodReturnTypeExtension
+{
+    public function __construct(private ReflectionProvider $reflectionProvider)
+    {
+    }
+
+    public function getClass(): string
+    {
+        return Collection::class;
+    }
+
+    public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return in_array($methodReflection->getName(), [
+            'make', 'wrap', 'times', 'range',
+        ], true);
+    }
+
+    public function getTypeFromStaticMethodCall(
+        MethodReflection $methodReflection,
+        StaticCall $methodCall,
+        Scope $scope
+    ): \PHPStan\Type\Type {
+        $returnType = ParametersAcceptorSelector::selectFromArgs(
+            $scope,
+            $methodCall->getArgs(),
+            $methodReflection->getVariants()
+        )->getReturnType();
+
+        if (! $returnType instanceof ObjectType && ! $returnType instanceof UnionType) {
+            return $returnType;
+        }
+
+        $class = $methodCall->class;
+
+        if (! $class instanceof Name) {
+            return new ErrorType();
+        }
+
+        $classReflection = $this->reflectionProvider->getClass((string) $class);
+
+        // If it's called on Support collection, just return.
+        if ($classReflection->getName() === Collection::class) {
+            return $returnType;
+        }
+
+        // If it's a UnionType, traverse the types and try to find a collection object type
+        if ($returnType instanceof UnionType) {
+            return $returnType->traverse(function (Type $type) use ($classReflection) {
+                if ($type instanceof GenericObjectType && (($innerReflection = $type->getClassReflection())) !== null) {
+                    return $this->handleGenericObjectType($classReflection, $innerReflection);
+                }
+
+                return $type;
+            });
+        }
+
+        $returnTypeClassReflection = $returnType->getClassReflection();
+
+        if ($returnTypeClassReflection === null) {
+            return $returnType;
+        }
+
+        return $this->handleGenericObjectType($classReflection, $returnTypeClassReflection);
+    }
+
+    private function handleGenericObjectType(ClassReflection $classReflection, ClassReflection $returnTypeClassReflection): GenericObjectType
+    {
+        $genericTypes = $returnTypeClassReflection->typeMapToList($returnTypeClassReflection->getActiveTemplateTypeMap());
+
+        $genericTypes = array_map(static function (Type $type) use ($classReflection) {
+            return TypeTraverser::map($type, static function (Type $type, callable $traverse) use ($classReflection): Type {
+                if ($type instanceof UnionType || $type instanceof IntersectionType) {
+                    return $traverse($type);
+                }
+
+                if ($type instanceof GenericObjectType && (($innerTypeReflection = $type->getClassReflection()) !== null)) {
+                    return new GenericObjectType($classReflection->getName(), $innerTypeReflection->typeMapToList($innerTypeReflection->getActiveTemplateTypeMap()));
+                }
+
+                return $traverse($type);
+            });
+        }, $genericTypes);
+
+        return new GenericObjectType($classReflection->getName(), $genericTypes);
+    }
+}
