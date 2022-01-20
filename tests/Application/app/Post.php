d2ae2deccc620cc62349acd89607597a9f347eac
Can Vural <can9119@gmail.com>
2022-01-20 23:58:16 +0100

    feat: add PostFactory

--- tests/Application/app/Post.php
+++ tests/Application/app/Post.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
+    use HasFactory;
+
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
---
+++ tests/Application/database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Post;
+use App\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
