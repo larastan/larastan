06e932b689c63b2e4780e0aaa6143f913896f574
Can Vural <can9119@gmail.com>
2022-02-25 11:34:54 +0100

    feat: add return type support for `$request->user()` call (#1136)

---
+++ tests/Type/GeneralTypeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Type;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+class GeneralTypeTest extends TypeInferenceTestCase
+{
+    /**
+     * @return iterable<mixed>
+     */
+    public function dataFileAsserts(): iterable
+    {
+        yield from $this->gatherAssertTypes(__DIR__.'/data/request-object.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     */
+    public function testFileAsserts(
+        string $assertType,
+        string $file,
+        ...$args
+    ): void {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__.'/../phpstan-tests.neon'];
+    }
+}
