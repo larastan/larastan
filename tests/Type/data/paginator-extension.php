fbdf481703706986832560f6569ddd5172e53814
Can Vural <can9119@gmail.com>
2022-02-28 15:25:27 +0100

    fix: update PHPStan version and stubs (#1144)

---
+++ tests/Type/data/paginator-extension.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaginatorExtension;
+
+use App\User;
+use function PHPStan\Testing\assertType;
+
+assertType('array', User::paginate()->all());
+assertType('array', User::simplePaginate()->all());
