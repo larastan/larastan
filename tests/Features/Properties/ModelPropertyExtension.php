68985b73bd51899532b6e05124055580cf9c3c47
Stephen Hamilton <totovste@googlemail.com>
2022-03-02 11:46:37 +0000

    Adds new test and updates old tests.

--- tests/Features/Properties/ModelPropertyExtension.php
+++ tests/Features/Properties/ModelPropertyExtension.php
@@ -189,6 +189,14 @@ class ModelPropertyExtension
         return $user->deleted_at?->format('d/m/Y');
     }
 
+    public function testWriteToSoftDeletesColumn(): void
+    {
+        $this->user->deleted_at = 'test';
+        $this->user->deleted_at = now();
+        $this->user->deleted_at = null;
+        $this->user->deleted_at = BaseCarbon::now();
+    }
+
     public function testForeignIdFor(Address $address): int
     {
         return $address->user_id;
