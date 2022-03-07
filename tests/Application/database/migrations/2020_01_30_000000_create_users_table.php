13e2c1c34541fad3b6f5ea4c20f2a9bd36942d85
Can Vural <can9119@gmail.com>
2022-03-07 13:24:09 +0100

    feat: support Laravel 9 style attribute accessors and mutators

--- tests/Application/database/migrations/2020_01_30_000000_create_users_table.php
+++ tests/Application/database/migrations/2020_01_30_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('stringButInt');
             $table->json('meta');
             $table->json('options');
             $table->json('properties');
--- tests/Type/GeneralTypeTest.php
+++ tests/Type/GeneralTypeTest.php
@@ -16,6 +16,7 @@ class GeneralTypeTest extends TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__.'/data/request-object.php');
         yield from $this->gatherAssertTypes(__DIR__.'/data/eloquent-builder.php');
         yield from $this->gatherAssertTypes(__DIR__.'/data/paginator-extension.php');
+        yield from $this->gatherAssertTypes(__DIR__.'/data/model-properties.php');
     }
 
     /**
@@ -31,6 +32,6 @@ class GeneralTypeTest extends TypeInferenceTestCase
 
     public static function getAdditionalConfigFiles(): array
     {
-        return [__DIR__.'/../phpstan-tests.neon'];
+        return [__DIR__.'/data/config-with-migrations.neon'];
     }
 }
