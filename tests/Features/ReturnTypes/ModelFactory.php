675d318313e4eef857ad30ea8f7117443933e133
Matthew Poulter <github@matthewpoulter.co.za>
2022-03-23 14:29:32 +0200

    feat improve model factory `after*` methods (#1195)

--- tests/Features/ReturnTypes/ModelFactory.php
+++ tests/Features/ReturnTypes/ModelFactory.php
@@ -55,6 +55,16 @@ class ModelFactory
         return User::factory()->unverified();
     }
 
+    public function testAfterMakingWithParentModelUsingFactory(): UserFactory
+    {
+        return User::factory()->afterMaking(fn (User $user) => $user);
+    }
+
+    public function testAfterCreatingWithParentModelUsingFactory(): UserFactory
+    {
+        return User::factory()->afterCreating(fn (User $user) => $user);
+    }
+
     public function testFactory(): PostFactory
     {
         return Post::factory();
@@ -91,5 +101,15 @@ class ModelFactory
     public function testMake()
     {
         return Post::factory()->make();
+    }
+
+    public function testAfterMaking(): PostFactory
+    {
+        return Post::factory()->afterMaking(fn (Post $post) => $post);
+    }
+
+    public function testAfterCreating(): PostFactory
+    {
+        return Post::factory()->afterCreating(fn (Post $post) => $post);
     }
 }
