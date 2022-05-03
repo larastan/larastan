1747a933e148ffb618eff92089b2129fedcbd57e
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-05-03 22:16:14 +0200

    Feat: add Rule checking for useless 'value()' function calls

---
+++ tests/Rules/Data/UselessConstructs/UselessValueFunctionCall.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Rules\Data\UselessConstructs;
+
+use function with;
+
+class UselessValueFunctionCall
+{
+    public function foo(): string
+    {
+        return value('foo');
+    }
+}
