6ba332997ae5c44ff73571851573c3128e1d2cbb
Christian N <c2n@live.dk>
2022-07-01 21:51:34 +0200

    fix

---
+++ tests/Type/data/blank-helper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BlankHelper;
+
+use function PHPStan\Testing\assertType;
+
+assertType('string', app()->environment());
+assertType('bool', app()->environment('local'));
+assertType('bool', app()->environment(['local', 'production']));
