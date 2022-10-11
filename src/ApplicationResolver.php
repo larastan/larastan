9ff89cd59301443906d0575edf217e10c5923032
Mior Muhammad Zaki <crynobone@gmail.com>
2022-10-11 15:19:38 +0800

    wip

--- src/ApplicationResolver.php
+++ src/ApplicationResolver.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace NunoMaduro\Larastan;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\PackageManifest;
 use Orchestra\Testbench\Foundation\Application as Testbench;
 use Orchestra\Testbench\Foundation\Config;
-use Illuminate\Foundation\PackageManifest as IlluminatePackageManifest;
 
 /**
  * @internal
@@ -24,7 +24,7 @@ final class ApplicationResolver
     public static function resolve(): Application
     {
         $resolvingCallback = function ($app) {
-            $packageManifest = $app->make(IlluminatePackageManifest::class);
+            $packageManifest = $app->make(PackageManifest::class);
 
             if (! file_exists($packageManifest->manifestPath)) {
                 $packageManifest->build();
