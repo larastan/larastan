21e66dc1fb198e68b253af36bc417f5e602c5ba1
Can Vural <can9119@gmail.com>
2022-10-23 17:17:32 +0200

    feat: unused views collector rule

---
+++ tests/Rules/Data/FooController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Rules\Data;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Mail\Mailable;
+
+class FooController
+{
+    public function existing(): View
+    {
+        return view('users.index');
+    }
+
+    public function existingNested(): View
+    {
+        return view('emails.orders.shipped');
+    }
+
+    public function notExisting(): View
+    {
+        return view('foo');
+    }
+}
+
+class FooEmail extends Mailable
+{
+    public function build(): self
+    {
+        return $this->markdown('emails.orders.shipped');
+    }
+
+    public function foo(): self
+    {
+        return $this->markdown('home');
+    }
+}
--- tests/Type/data/view.php
+++ tests/Type/data/view.php
@@ -4,6 +4,7 @@ namespace IlluminateView;
 
 use function PHPStan\Testing\assertType;
 
+assertType('Illuminate\Contracts\View\Factory', view());
 assertType('Illuminate\View\View', view('foo'));
 assertType('Illuminate\View\View', view('foo')->with('bar', 'baz'));
 assertType('Illuminate\View\View', view('foo')->withFoo('bar'));
