1929e3219a078dcfe3345740fd0b14dbc3942bee
Viktor Szépe <viktor@szepe.net>
2023-01-24 16:36:06 +0000

    Fake PR to test CI ✔

--- tests/Type/data/collection-generic-static-methods.php
+++ tests/Type/data/collection-generic-static-methods.php
@@ -13,7 +13,7 @@ use function PHPStan\Testing\assertType;
 /** @var App\UserCollection $secondCustomEloquentCollection */
 /** @var User $user */
 assertType('Illuminate\Database\Eloquent\Collection<int, int>', EloquentCollection::range(1, 10));
-
+// Fake PR
 assertType('Illuminate\Support\Collection<int, mixed>', $collection->collapse());
 assertType('Illuminate\Support\Collection<int, mixed>', $items->collapse());
 
