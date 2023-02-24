b88e4d3ebe5c0678ffde878ca90106618c80324c
Benedikt Franke <benedikt.franke@mll.com>
2023-02-24 15:37:31 +0100

    Fix integration data path

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -12,9 +12,9 @@ class IntegrationTest extends PHPStanTestCase
      */
     public function dataIntegrationTests(): iterable
     {
-        yield [__DIR__.'/integration/test-case-extension.php'];
-        yield [__DIR__.'/integration/model-properties.php'];
-        yield [__DIR__.'/integration/blade-view.php'];
+        yield [__DIR__.'/data/test-case-extension.php'];
+        yield [__DIR__.'/data/model-properties.php'];
+        yield [__DIR__.'/data/blade-view.php'];
     }
 
     /**
