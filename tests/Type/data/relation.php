87299813881e86607408ce303da90515a851b721
René Bos <rene.bos@visma.com>
2023-03-21 14:35:30 +0100

    Update Relation stub with more methods

---
+++ tests/Type/data/relation.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Relation;
+
+use App\Post;
+use function PHPStan\Testing\assertType;
+
+$relation = (new Post())->users();
+
+assertType('Illuminate\Database\Eloquent\Collection<int, App\User>', $relation->getEager());
+assertType('Illuminate\Database\Eloquent\Collection<int, App\User>', $relation->get());
+assertType('Illuminate\Database\Eloquent\Builder<App\User>', $relation->getQuery());
+assertType('App\User',  $relation->make());
