e83942505dc0949d1e8ce081d5843e2cde720d7d
Fabien Villepinte <fabien.villepinte@gmail.com>
2023-04-02 21:12:22 +0200

    fix: internal error with macros defined by a string (#1595)

--- tests/Type/data/macros.php
+++ tests/Type/data/macros.php
@@ -9,10 +9,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use PHPStan\TrinaryLogic;
+
 use function PHPStan\Testing\assertType;
 use function PHPStan\Testing\assertVariableCertainty;
-use PHPStan\TrinaryLogic;
 
 try {
     Request::validate([]);
@@ -30,5 +32,8 @@ assertType('int', Auth::requestGuardMacro());
 
 assertType('string', collect([])->customCollectionMacro());
 assertType('string', Collection::customCollectionMacro());
+
+assertType('string', Str::trimMacro(''));
+assertType('string', Str::asciiAliasMacro(''));
 
 assertVariableCertainty(TrinaryLogic::createMaybe(), $foo);
