b70e369ea2cec6c1967f4cf0c50d8fe170eafe8f
Erik Gaal <me@erikgaal.nl>
2023-04-11 21:26:29 +0200

    feat: add `Tappable` stub (#1586)

---
+++ tests/Type/data/tappable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tappable;
+
+use Illuminate\Support\Traits\Tappable;
+use function PHPStan\Testing\assertType;
+
+class TappableClass
+{
+    use Tappable;
+}
+
+assertType(
+    'Tappable\TappableClass',
+    (new TappableClass())->tap(function (TappableClass $tappable) {
+    }),
+);
+
+assertType(
+    'Illuminate\Support\HigherOrderTapProxy<Tappable\TappableClass>',
+    (new TappableClass())->tap(),
+);
