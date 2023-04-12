0a7895ea8e02268f6017f858021250334a382eed
tpetry <github@tpetry.me>
2023-04-12 11:00:07 +0200

    fixes

--- tests/Type/data/request-header.php
+++ tests/Type/data/request-header.php
@@ -7,6 +7,6 @@ namespace RequestHeader;
 use function PHPStan\Testing\assertType;
 
 /** @var \Illuminate\Http\Request $request */
-assertType('array<string, string>', $request->header());
+assertType('array<string, array<int, string|null>>', $request->header());
 assertType('string|null', $request->header('key'));
 assertType('string', $request->header('key', 'default'));
