c8f79b9933c1a1bba7587f3cb56de62a028014d1
Vytautas Smilingis <vytautas@whatagraph.com>
2023-07-21 16:42:41 +0300

    Added test to verify that `retry()` accepts callable as `$sleepMilliseconds`

--- tests/Integration/data/helpers.php
+++ tests/Integration/data/helpers.php
@@ -6,3 +6,14 @@ function transformAcceptsNonNullableCallable(): void
 {
     transform(User::first(), fn (User $user) => $user->toArray());
 }
+
+function retryAcceptsCallableAsSleepMilliseconds(): void
+{
+    retry(5, function (int $attempt): bool {
+        return false;
+    }, function (int $attempt, Exception $e): int {
+        return 0;
+    }, function (Exception $e): bool {
+        return true;
+    });
+}
