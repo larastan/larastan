891568130bd82766f3120fe4c510bd13a94d5e32
Robert van Steen <hello@rovansteen.nl>
2023-10-10 09:36:59 +0200

    add failing test

--- tests/Type/data/collection-helper.php
+++ tests/Type/data/collection-helper.php
@@ -44,3 +44,31 @@ function testIterable(\Traversable $foo): void
 {
     assertType('Illuminate\Support\Collection<int, int>', collect($foo));
 }
+
+// Something with generics
+/**
+ * @template T
+ */
+class Ok {
+    /**
+     * @param T $value
+     */
+    public function __construct(public mixed $value) {
+    }
+}
+
+/**
+ * @template T
+ */
+class Some
+{
+    /**
+     * @param T $value
+     */
+    public function __construct(public mixed $value)
+    {
+    }
+}
+
+assertType('Illuminate\Support\Collection<int, CollectionHelper\Ok<CollectionHelper\Some<int>>>', collect([new Ok(new Some(42))]));
+assertType('Illuminate\Support\Collection<int, CollectionHelper\Ok<CollectionHelper\Some<int>>>', collect([42])->map(fn (int $i) => new Ok(new Some($i))));
