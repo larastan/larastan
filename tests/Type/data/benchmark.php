1fb9641e55c9ad8a47546b2cbd2ce7c1385f0359
Jesper Beisner <jesper.beisner+github@gmail.com>
2023-12-06 21:57:53 +0100

    feat: Add new Benchmark.stub

---
+++ tests/Type/data/benchmark.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Benchmark;
+
+use Illuminate\Support\Benchmark;
+
+use function PHPStan\Testing\assertType;
+
+function doFoo()
+{
+    assertType('float', Benchmark::measure(fn () => 'Hello World'));
+    assertType('array<int, float>', Benchmark::measure([fn () => 'Hello World', fn () => 'Hello World']));
+    assertType('array<string, float>', Benchmark::measure(['test1' => fn () => 'Hello World', 'test2' => fn () => 'Hello World']));
+    assertType('array<int|string, float>', Benchmark::measure(['test' => fn () => 'Hello World', 100 => fn () => 'Hello World']));
+}
