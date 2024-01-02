b4e636cd6623ae208ec0ec4463d70f86a04b2924
Jesper Beisner <jesper.beisner+github@gmail.com>
2024-01-02 22:58:25 +0100

    feat: Add new Benchmark.stub (#1796)
    Co-authored-by: Can Vural <can9119@gmail.com>

--- tests/Type/GeneralTypeTest.php
+++ tests/Type/GeneralTypeTest.php
@@ -55,6 +55,7 @@ class GeneralTypeTest extends TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__.'/data/helpers.php');
         yield from $this->gatherAssertTypes(__DIR__.'/data/custom-eloquent-collection.php');
         yield from $this->gatherAssertTypes(__DIR__.'/data/translator.php');
+        yield from $this->gatherAssertTypes(__DIR__.'/data/benchmark.php');
         yield from $this->gatherAssertTypes(__DIR__.'/data/bug-1760.php');
 
         if (version_compare(LARAVEL_VERSION, '10.20.0', '>=')) {
