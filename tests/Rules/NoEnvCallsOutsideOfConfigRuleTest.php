4ea62d99d792b86df1160d14a373698ad1537aab
Caleb White <cdwhite3@pm.me>
2024-02-06 23:34:22 -0600

    feat: create noEnvCallsOutsideConfig rule

---
+++ tests/Rules/NoEnvCallsOutsideOfConfigRuleTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Rules;
+
+use Illuminate\Foundation\Application;
+use Larastan\Larastan\Rules\NoEnvCallsOutsideOfConfigRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use ReflectionClass;
+
+/** @extends RuleTestCase<NoEnvCallsOutsideOfConfigRule> */
+class NoEnvCallsOutsideOfConfigRuleTest extends RuleTestCase
+{
+    protected function setUp(): void
+    {
+        $this->overrideConfigPath(__DIR__.'/data/config');
+    }
+
+    protected function getRule(): Rule
+    {
+        return new NoEnvCallsOutsideOfConfigRule();
+    }
+
+    /** @test */
+    public function itDoesNotFailForEnvCallsInsideConfigDirectory(): void
+    {
+        $this->analyse([__DIR__.'/data/config/env-calls.php'], []);
+    }
+
+    /** @test */
+    public function itReportsEnvCallsOutsideOfConfigDirectory(): void
+    {
+        $this->analyse([__DIR__.'/data/env-calls.php'], [
+            ["Called 'env' outside of the config directory which returns null when the config is cached, use 'config'.", 7],
+            ["Called 'env' outside of the config directory which returns null when the config is cached, use 'config'.", 8],
+        ]);
+    }
+
+
+    protected function overrideConfigPath(string $path): void
+    {
+        $app = Application::getInstance();
+
+        if (version_compare(LARAVEL_VERSION, '10.0.0', '>=')) {
+            $app->useConfigPath($path);
+
+            return;
+        }
+
+        $reflectionClass = new ReflectionClass($app);
+        $property = $reflectionClass->getProperty('basePath');
+        $property->setAccessible(true);
+
+        $property->setValue($app, str_replace('/config', '', $path));
+    }
+}
