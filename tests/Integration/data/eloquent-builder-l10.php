2ac60334eb7e5d693df9a9150bd6798be680f99c
Caleb White <cdwhite3@pm.me>
2024-03-09 22:45:33 -0600

    fix: subquery ordering (#1875)

--- tests/Integration/data/eloquent-builder-l10.php
+++ tests/Integration/data/eloquent-builder-l10.php
@@ -2,7 +2,12 @@
 
 namespace EloquentBuilderLaravel10;
 
+use App\Post;
 use App\User;
 use Illuminate\Support\Facades\DB;
 
 User::query()->where(DB::raw('1'), 1)->get();
+
+/** @see https://github.com/larastan/larastan/issues/1806 */
+User::query()->orderBy(Post::query()->select('id')->whereColumn('user_id', 'users.id'));
+User::query()->orderByDesc(Post::query()->select('id')->whereColumn('user_id', 'users.id'));
