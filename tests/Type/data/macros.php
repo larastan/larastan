fdc7d295320a2040d4e34a8e6c406dae5123b68d
Can Vural <can9119@gmail.com>
2024-05-15 19:02:58 +0200

    feat: specify this type for all macros

--- tests/Type/data/macros.php
+++ tests/Type/data/macros.php
@@ -11,39 +11,56 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
 use Illuminate\Validation\ValidationException;
 use PHPStan\TrinaryLogic;
 
 use function PHPStan\Testing\assertType;
 use function PHPStan\Testing\assertVariableCertainty;
 
-try {
-    Request::validate([]);
-} catch (ValidationException $e) {
-    $foo = 'foo';
+function test(): void
+{
+    try {
+        Request::validate([]);
+    } catch (ValidationException $e) {
+        $foo = 'foo';
+    }
+
+    assertType('string', Builder::globalCustomMacro(b: 99));
+    assertType('string', Post::globalCustomMacro(b: 99));
+    assertType('string', PostBuilder::globalCustomMacro(b: 99));
+    assertType('string', User::first()->accounts()->globalCustomMacro(b: 99));
+
+    assertType('string', \Illuminate\Database\Query\Builder::globalCustomDatabaseQueryMacro(b: 99));
+    assertType('string', Post::globalCustomDatabaseQueryMacro(b: 99));
+    assertType('string', PostBuilder::globalCustomDatabaseQueryMacro(b: 99));
+    assertType('string', User::first()->accounts()->globalCustomDatabaseQueryMacro(b: 99));
+
+    assertType('int', Route::facadeMacro());
+    assertType('int', Auth::sessionGuardMacro());
+    assertType('int', Auth::requestGuardMacro());
+
+    assertType('string', collect([])->customCollectionMacro());
+    assertType('string', Collection::customCollectionMacro());
+
+    assertType('string', collect([])->customCollectionMacroString());
+    assertType('string', Collection::customCollectionMacroString());
+
+    assertType('string', Str::trimMacro(''));
+    assertType('string', Str::asciiAliasMacro(''));
+
+    assertVariableCertainty(TrinaryLogic::createMaybe(), $foo);
+
+    Request::macro('foo', function() {
+        assertType(Request::class, $this);
+    });
+
+    Foo::macro('foo', function() {
+        assertType(Foo::class, $this);
+    });
 }
 
-assertType('string', Builder::globalCustomMacro(b: 99));
-assertType('string', Post::globalCustomMacro(b: 99));
-assertType('string', PostBuilder::globalCustomMacro(b: 99));
-assertType('string', User::first()->accounts()->globalCustomMacro(b: 99));
-
-assertType('string', \Illuminate\Database\Query\Builder::globalCustomDatabaseQueryMacro(b: 99));
-assertType('string', Post::globalCustomDatabaseQueryMacro(b: 99));
-assertType('string', PostBuilder::globalCustomDatabaseQueryMacro(b: 99));
-assertType('string', User::first()->accounts()->globalCustomDatabaseQueryMacro(b: 99));
-
-assertType('int', Route::facadeMacro());
-assertType('int', Auth::sessionGuardMacro());
-assertType('int', Auth::requestGuardMacro());
-
-assertType('string', collect([])->customCollectionMacro());
-assertType('string', Collection::customCollectionMacro());
-
-assertType('string', collect([])->customCollectionMacroString());
-assertType('string', Collection::customCollectionMacroString());
-
-assertType('string', Str::trimMacro(''));
-assertType('string', Str::asciiAliasMacro(''));
-
-assertVariableCertainty(TrinaryLogic::createMaybe(), $foo);
+class Foo
+{
+    use Macroable;
+}
