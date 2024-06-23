814cf10a7efec9328936b3705ea0c8390b07e588
Rob Chettleburgh <rob@evosite.co.uk>
2024-06-23 11:26:50 +0000

    Feat: infer type of guard for auth('...')

--- src/ReturnTypes/Helpers/AuthExtension.php
+++ src/ReturnTypes/Helpers/AuthExtension.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Larastan\Larastan\ReturnTypes\Helpers;
 
+use Illuminate\Auth\RequestGuard;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Auth\TokenGuard;
 use Illuminate\Contracts\Auth\Factory;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\StatefulGuard;
@@ -16,7 +19,9 @@ use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 
+use function array_key_exists;
 use function count;
+use function is_string;
 
 /** @internal */
 final class AuthExtension implements DynamicFunctionReturnTypeExtension
@@ -44,6 +49,55 @@ final class AuthExtension implements DynamicFunctionReturnTypeExtension
             return new ObjectType($class::class);
         }
 
-        return TypeCombinator::intersect(new ObjectType(Guard::class), new ObjectType(StatefulGuard::class));
+        return $this->getTypeFromStaticGuardCall($functionCall, $scope);
+    }
+
+    public function getTypeFromStaticGuardCall(
+        FuncCall $methodCall,
+        Scope $scope,
+    ): Type {
+        $defaultReturnType = TypeCombinator::intersect(new ObjectType(Guard::class), new ObjectType(StatefulGuard::class));
+
+        $config = $this->getContainer()->get('config');
+        if ($config === null) {
+            return $defaultReturnType;
+        }
+
+        /** @var array<string, mixed> $guards */
+        $guards = $config->get('auth.guards');
+        /** @var string $defaultGuard */
+        $defaultGuard = $config->get('auth.defaults.guard');
+
+        if (count($methodCall->getArgs()) === 0) {
+            if (! array_key_exists($defaultGuard, $guards)) {
+                return $defaultReturnType;
+            }
+
+            return $this->findTypeFromGuardDriver($guards[$defaultGuard]['driver']) ?? $defaultReturnType;
+        }
+
+        $argType    = $scope->getType($methodCall->getArgs()[0]->value);
+        $argStrings = $argType->getConstantStrings();
+
+        if (count($argStrings) !== 1) {
+            return $defaultReturnType;
+        }
+
+        $driver = $config->get('auth.guards.' . $argStrings[0]->getValue() . '.driver', null);
+        if (! is_string($driver)) {
+            return $defaultReturnType;
+        }
+
+        return $this->findTypeFromGuardDriver($driver) ?? $defaultReturnType;
+    }
+
+    private function findTypeFromGuardDriver(string $driver): Type|null
+    {
+        return match ($driver) {
+            'session' => new ObjectType(SessionGuard::class),
+            'token' => new ObjectType(TokenGuard::class),
+            'passport' => new ObjectType(RequestGuard::class),
+            default => null,
+        };
     }
 }
--- tests/Type/data/auth.php
+++ tests/Type/data/auth.php
@@ -5,6 +5,7 @@ namespace Auth;
 use App\User;
 use Illuminate\Auth\SessionGuard;
 use Illuminate\Auth\TokenGuard;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,15 +13,30 @@ use function PHPStan\Testing\assertType;
 
 function test(User $user): void
 {
-    assertType('App\Admin|App\User|null', Auth::user());
     assertType('bool', Auth::check());
+
     assertType('int|string|null', Auth::id());
-    assertType('null', Auth::guard()->logout());
-    assertType(SessionGuard::class, Auth::guard('web'));
-    assertType('App\User|null', Auth::guard('web')->user());
-    assertType('App\Admin|null', Auth::guard('admin')->user());
-    assertType(TokenGuard::class, Auth::guard('api'));
-    assertType(SessionGuard::class, Auth::guard('undefined'));
-    assertType('null', Auth::guard()->login($user));
     assertType('App\Admin|App\User', Auth::authenticate());
+    assertType('null', Auth::guard()->logout());
+    assertType('null', Auth::guard()->login($user));
+
+    assertType(SessionGuard::class, Auth::guard('web'));
+    assertType('int|string|null', Auth::guard('web')->id());
+    assertType('App\User|null', Auth::guard('web')->user());
+    assertType('App\User', Auth::guard('web')->authenticate());
+    assertType('null', Auth::guard('web')->logout());
+
+    assertType(SessionGuard::class, Auth::guard('admin'));
+    assertType('int|string|null', Auth::guard('admin')->id());
+    assertType('App\Admin|null', Auth::guard('admin')->user());
+    assertType('App\Admin', Auth::guard('admin')->authenticate());
+
+    assertType(TokenGuard::class, Auth::guard('api'));
+    assertType('int|string|null', Auth::guard('api')->id());
+    assertType('App\User|null', Auth::guard('api')->user());
+    assertType('App\User', Auth::guard('api')->authenticate());
+
+    assertType(StatefulGuard::class, Auth::guard('unknown'));
+    assertType('int|string|null', Auth::guard('unknown')->id());
+    assertType(Authenticatable::class . '|null', Auth::guard('unknown')->user());
 }
