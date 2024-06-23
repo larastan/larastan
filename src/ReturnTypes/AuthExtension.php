98608011a4ae8c25e46531a5177bbda43257f618
Rob Chettleburgh <rob@evosite.co.uk>
2024-06-23 11:26:50 +0000

    Feat: infer type of guard for auth('...')

--- src/ReturnTypes/AuthExtension.php
+++ src/ReturnTypes/AuthExtension.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Larastan\Larastan\ReturnTypes;
 
+use Exception;
+use Illuminate\Auth\RequestGuard;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Auth\TokenGuard;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Support\Facades\Auth;
 use Larastan\Larastan\Concerns;
 use PhpParser\Node\Expr\StaticCall;
@@ -15,9 +21,11 @@ use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 
+use function array_key_exists;
 use function array_map;
 use function count;
 use function in_array;
+use function is_string;
 
 /** @internal */
 final class AuthExtension implements DynamicStaticMethodReturnTypeExtension
@@ -32,13 +40,24 @@ final class AuthExtension implements DynamicStaticMethodReturnTypeExtension
 
     public function isStaticMethodSupported(MethodReflection $methodReflection): bool
     {
-        return in_array($methodReflection->getName(), ['user', 'authenticate']);
+        return in_array($methodReflection->getName(), ['user', 'authenticate', 'guard']);
     }
 
     public function getTypeFromStaticMethodCall(
         MethodReflection $methodReflection,
         StaticCall $methodCall,
         Scope $scope,
+    ): Type {
+        return match ($methodReflection->getName()) {
+            'authenticate' => $this->getTypeFromStaticAuthenticateCall($methodReflection),
+            'user' => $this->getTypeFromStaticUserCall($methodReflection),
+            'guard' => $this->getTypeFromStaticGuardCall($methodCall, $scope),
+            default => throw new Exception('Unhandled static method return type extension' . $methodReflection->getName())
+        };
+    }
+
+    public function getTypeFromStaticAuthenticateCall(
+        MethodReflection $methodReflection,
     ): Type {
         $config     = $this->getContainer()->get('config');
         $authModels = [];
@@ -55,10 +74,62 @@ final class AuthExtension implements DynamicStaticMethodReturnTypeExtension
             static fn (string $authModel): Type => new ObjectType($authModel),
             $authModels,
         ));
-        if ($methodReflection->getName() === 'user') {
-            $type = TypeCombinator::addNull($type);
-        }
 
         return $type;
+    }
+
+    public function getTypeFromStaticUserCall(
+        MethodReflection $methodReflection,
+    ): Type {
+        return TypeCombinator::addNull($this->getTypeFromStaticAuthenticateCall($methodReflection));
+    }
+
+    public function getTypeFromStaticGuardCall(
+        StaticCall $methodCall,
+        Scope $scope,
+    ): Type {
+        $defaultReturnType = TypeCombinator::intersect(new ObjectType(Guard::class), new ObjectType(StatefulGuard::class));
+
+        $config = $this->getContainer()->get('config');
+        if ($config === null) {
+            return $defaultReturnType;
+        }
+
+        /** @var array<string, mixed> $guards */
+        $guards = $config->get('auth.guards');
+        /** @var string $defaultGuard */
+        $defaultGuard = $config->get('auth.defaults.guard');
+
+        if (count($methodCall->getArgs()) === 0) {
+            if (! array_key_exists($defaultGuard, $guards)) {
+                return $defaultReturnType;
+            }
+
+            return $this->findTypeFromGuardDriver($guards[$defaultGuard]['driver']) ?? $defaultReturnType;
+        }
+
+        $argType    = $scope->getType($methodCall->getArgs()[0]->value);
+        $argStrings = $argType->getConstantStrings();
+
+        if (count($argStrings) !== 1) {
+            return $defaultReturnType;
+        }
+
+        $driver = $config->get('auth.guards.' . $argStrings[0]->getValue() . '.driver', null);
+        if (! is_string($driver)) {
+            return $defaultReturnType;
+        }
+
+        return $this->findTypeFromGuardDriver($driver) ?? $defaultReturnType;
+    }
+
+    private function findTypeFromGuardDriver(string $driver): Type|null
+    {
+        return match ($driver) {
+            'session' => new ObjectType(SessionGuard::class),
+            'token' => new ObjectType(TokenGuard::class),
+            'passport' => new ObjectType(RequestGuard::class),
+            default => null,
+        };
     }
 }
--- src/ReturnTypes/AuthManagerExtension.php
+++ src/ReturnTypes/AuthManagerExtension.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Larastan\Larastan\ReturnTypes;
 
+use Exception;
 use Illuminate\Auth\AuthManager;
+use Illuminate\Auth\RequestGuard;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Auth\TokenGuard;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Larastan\Larastan\Concerns;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
@@ -15,9 +21,11 @@ use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 
+use function array_key_exists;
 use function array_map;
 use function count;
 use function in_array;
+use function is_string;
 
 final class AuthManagerExtension implements DynamicMethodReturnTypeExtension
 {
@@ -31,13 +39,24 @@ final class AuthManagerExtension implements DynamicMethodReturnTypeExtension
 
     public function isMethodSupported(MethodReflection $methodReflection): bool
     {
-        return in_array($methodReflection->getName(), ['user', 'authenticate']);
+        return in_array($methodReflection->getName(), ['user', 'authenticate', 'guard']);
     }
 
     public function getTypeFromMethodCall(
         MethodReflection $methodReflection,
         MethodCall $methodCall,
         Scope $scope,
+    ): Type {
+               return match ($methodReflection->getName()) {
+            'authenticate' => $this->getTypeFromAuthenticateCall($methodReflection),
+            'user' => $this->getTypeFromUserCall($methodReflection),
+            'guard' => $this->getTypeFromGuardCall($methodCall, $scope),
+            default => throw new Exception('Unhandled static method return type extension' . $methodReflection->getName())
+               };
+    }
+
+    public function getTypeFromAuthenticateCall(
+        MethodReflection $methodReflection,
     ): Type {
         $config     = $this->getContainer()->get('config');
         $authModels = [];
@@ -54,10 +73,62 @@ final class AuthManagerExtension implements DynamicMethodReturnTypeExtension
             static fn (string $authModel): Type => new ObjectType($authModel),
             $authModels,
         ));
-        if ($methodReflection->getName() === 'user') {
-            $type = TypeCombinator::addNull($type);
-        }
 
         return $type;
+    }
+
+    public function getTypeFromUserCall(
+        MethodReflection $methodReflection,
+    ): Type {
+        return TypeCombinator::addNull($this->getTypeFromAuthenticateCall($methodReflection));
+    }
+
+    public function getTypeFromGuardCall(
+        MethodCall $methodCall,
+        Scope $scope,
+    ): Type {
+        $defaultReturnType = TypeCombinator::intersect(new ObjectType(Guard::class), new ObjectType(StatefulGuard::class));
+
+        $config = $this->getContainer()->get('config');
+        if ($config === null) {
+            return $defaultReturnType;
+        }
+
+        /** @var array<string, mixed> $guards */
+        $guards = $config->get('auth.guards');
+        /** @var string $defaultGuard */
+        $defaultGuard = $config->get('auth.defaults.guard');
+
+        if (count($methodCall->getArgs()) === 0) {
+            if (! array_key_exists($defaultGuard, $guards)) {
+                return $defaultReturnType;
+            }
+
+            return $this->findTypeFromGuardDriver($guards[$defaultGuard]['driver']) ?? $defaultReturnType;
+        }
+
+        $argType    = $scope->getType($methodCall->getArgs()[0]->value);
+        $argStrings = $argType->getConstantStrings();
+
+        if (count($argStrings) !== 1) {
+            return $defaultReturnType;
+        }
+
+        $driver = $config->get('auth.guards.' . $argStrings[0]->getValue() . '.driver', null);
+        if (! is_string($driver)) {
+            return $defaultReturnType;
+        }
+
+        return $this->findTypeFromGuardDriver($driver) ?? $defaultReturnType;
+    }
+
+    private function findTypeFromGuardDriver(string $driver): Type|null
+    {
+        return match ($driver) {
+            'session' => new ObjectType(SessionGuard::class),
+            'token' => new ObjectType(TokenGuard::class),
+            'passport' => new ObjectType(RequestGuard::class),
+            default => null,
+        };
     }
 }
