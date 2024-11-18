23c9305d9972efa8af64ed9b86a8d8d7f0bfdab1
Rudie Dirkx <github@hotblocks.nl>
2024-11-19 00:04:52 +0100

    cs

--- src/Methods/CollectionChunkClosureTypeExtension.php
+++ src/Methods/CollectionChunkClosureTypeExtension.php
@@ -8,20 +8,20 @@ use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Larastan\Larastan\Internal\LaravelVersion;
 use Larastan\Larastan\Support\CollectionHelper;
+use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\Native\NativeParameterReflection;
 use PHPStan\Reflection\ParameterReflection;
 use PHPStan\Reflection\PassedByReference;
-use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\ClosureType;
-use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\MethodParameterClosureTypeExtension;
 use PHPStan\Type\MixedType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
-use PhpParser\Node\Expr\MethodCall;
+
+use function in_array;
 
 final class CollectionChunkClosureTypeExtension implements MethodParameterClosureTypeExtension
 {
@@ -31,33 +31,37 @@ final class CollectionChunkClosureTypeExtension implements MethodParameterClosur
 
     public function isMethodSupported(MethodReflection $methodReflection, ParameterReflection $parameter): bool
     {
-        if (!in_array($methodReflection->getName(), ['chunk', 'chunkById', 'chunkByIdDesc'])) {
+        if (! in_array($methodReflection->getName(), ['chunk', 'chunkById', 'chunkByIdDesc'], strict: true)) {
             return false;
         }
 
-        if ($parameter->getName() != 'callback') {
+        if ($parameter->getName() !== 'callback') {
             return false;
         }
 
-        return (new ObjectType(EloquentBuilder::class))->isSuperTypeOf(new ObjectType($methodReflection->getDeclaringClass()->getName()))->yes();
+        return $methodReflection->getDeclaringClass()->is(EloquentBuilder::class);
     }
 
     public function getTypeFromMethodCall(
         MethodReflection $methodReflection,
         MethodCall $methodCall,
         ParameterReflection $parameter,
-        Scope $scope
+        Scope $scope,
     ): Type|null {
         $modelClassType = $methodReflection->getDeclaringClass()->getActiveTemplateTypeMap()->getType(LaravelVersion::getBuilderModelGenericName());
+        if (! $modelClassType || ! $modelClassType->getObjectClassNames()) {
+            return null;
+        }
 
         if ((new ObjectType(Model::class))->isSuperTypeOf($modelClassType)->no()) {
             return null;
         }
 
-        $modelClassName = $modelClassType->getClassName();
+        $modelClassName = $modelClassType->getObjectClassNames()[0];
         $collectionType = $this->collectionHelper->determineCollectionClass($modelClassName);
 
         $notByReference = PassedByReference::createNo();
+
         return new ClosureType(
             [
                 new NativeParameterReflection(
@@ -80,5 +84,4 @@ final class CollectionChunkClosureTypeExtension implements MethodParameterClosur
             new MixedType(),
         );
     }
-
 }
