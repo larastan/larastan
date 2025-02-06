afa69c5d7874f19fd4ccb811e8cf17f94cd22545
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-06 22:01:48 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Type/GeneralTypeTest.php
+++ tests/Type/GeneralTypeTest.php
@@ -82,7 +82,7 @@ class GeneralTypeTest extends TypeInferenceTestCase
             yield from self::gatherAssertTypes(__DIR__ . '/data/model-relations.php');
         }
 
-        if (laravel_version_compare('11.0.0', '>=') && laravel_version_compare('12.0.0', '<')) {
+        if (laravel_version_compare('11.0.0', '>=') && ! laravel_version_compare('12.0.0', '>=')) {
             yield from self::gatherAssertTypes(__DIR__ . '/data/collection-generic-static-methods-l11.php');
         }
 
