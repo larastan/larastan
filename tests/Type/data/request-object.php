6863e2bf34c36707fc73d13b4df44003e38e6f01
Can Vural <can9119@gmail.com>
2025-03-28 11:20:45 +0100

    feat: add InteractsWithInput stub

--- tests/Type/data/request-object.php
+++ tests/Type/data/request-object.php
@@ -11,9 +11,12 @@ use function PHPStan\Testing\assertType;
 function test(Request $request): void
 {
     assertType('array<int, Illuminate\Http\UploadedFile>', $request->file());
-    assertType('array<int, Illuminate\Http\UploadedFile>|Illuminate\Http\UploadedFile|null', $request->file('foo'));
+    assertType('Illuminate\Http\UploadedFile|null', $request->file('foo'));
     assertType('array<int, Illuminate\Http\UploadedFile>|Illuminate\Http\UploadedFile|stdClass', $request->file('foo', new \stdClass()));
     assertType('Illuminate\Routing\Route|null', $request->route());
-    assertType('object|string|null', $request->route('foo'));
-    assertType('object|string|null', $request->route('foo', 'bar'));
+    assertType('(object|string|null)', $request->route('foo'));
+    assertType('(object|string)', $request->route('foo', 'bar'));
+    assertType('array<string, string>', $request->server());
+    assertType('string|null', $request->server('foo'));
+    assertType('int|string', $request->server('foo', 5));
 }
