a8473596c96fae3b71e89fae0023b9b867a9a9ce
Can Vural <can9119@gmail.com>
2025-03-28 11:20:07 +0100

    feat: add new extension for App facade environment method

---
+++ src/ReturnTypes/AppFacadeEnvironmentReturnTypeExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Larastan\Larastan\ReturnTypes;
+
+use Illuminate\Support\Facades\App;
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use function count;
+
+class AppFacadeEnvironmentReturnTypeExtension implements DynamicStaticMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return App::class;
+    }
+
+    public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'environment';
+    }
+
+    public function getTypeFromStaticMethodCall(
+        MethodReflection $methodReflection,
+        StaticCall $methodCall,
+        Scope $scope,
+    ): Type {
+        if (count($methodCall->getArgs()) === 0) {
+            return new StringType();
+        }
+
+        return new BooleanType();
+    }
+}
