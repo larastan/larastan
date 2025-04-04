7ea1d44e663ce77215dd1f491b79c83511fa20c2
Liam Duckett <liamduckett2@gmail.com>
2025-04-04 22:19:47 +0100

    simplify test

--- tests/Type/data/arrayable.php
+++ tests/Type/data/arrayable.php
@@ -3,18 +3,10 @@
 namespace Arrayable;
 
 use App\ValueObjects\Foo;
-use App\ValueObjects\FooList;
 use function PHPStan\Testing\assertType;
 
 function test(): void
 {
     $foo = new Foo;
     assertType('array<string, int>', $foo->toArray());
-
-    $fooList = new FooList;
-    $array = $fooList->toArray();
-    assertType('array<int, Illuminate\Contracts\Support\Arrayable<string, int>>', $array);
-    $fooFromArray = $array[0];
-    assertType('Illuminate\Contracts\Support\Arrayable<string, int>', $fooFromArray);
-    assertType('array<string, int>', $fooFromArray->toArray());
 }
