93ad280248e4142ac7ead2e0351af4db9f77d943
Rik van der Heijden <rik.vanderheijden@zoolt.com>
2025-04-24 09:18:23 +0200

    extend test for InteractsWithInput

--- tests/Type/data/request-object.php
+++ tests/Type/data/request-object.php
@@ -13,10 +13,18 @@ function test(Request $request): void
     assertType('array<int, Illuminate\Http\UploadedFile>', $request->file());
     assertType('Illuminate\Http\UploadedFile|null', $request->file('foo'));
     assertType('array<int, Illuminate\Http\UploadedFile>|Illuminate\Http\UploadedFile|stdClass', $request->file('foo', new \stdClass()));
+
     assertType('Illuminate\Routing\Route|null', $request->route());
     assertType('(object|string|null)', $request->route('foo'));
     assertType('(object|string)', $request->route('foo', 'bar'));
+
     assertType('array<string, string>', $request->server());
     assertType('string|null', $request->server('foo'));
     assertType('5|string', $request->server('foo', 5));
+
+    assertType('array|string|null', $request->post('foo'));
+    assertType('array|string', $request->post('foo', 'bar'));
+
+    assertType('array|string|null', $request->query('foo'));
+    assertType('array|string', $request->query('foo', 'bar'));
 }
