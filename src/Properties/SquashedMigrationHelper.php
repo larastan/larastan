574eeebc920915f0bd30e567d3c5489836bae70d
pataar <pietering1@gmail.com>
2025-04-24 15:36:10 +0200

    resolve phpcs

--- src/Properties/SquashedMigrationHelper.php
+++ src/Properties/SquashedMigrationHelper.php
@@ -16,6 +16,7 @@ use SplFileInfo;
 use function array_key_exists;
 use function database_path;
 use function file_get_contents;
+use function glob;
 use function is_array;
 use function is_bool;
 use function is_dir;
