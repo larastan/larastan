b94eb6d115983951e20f88a7c3667dbf8f062ff9
Rik van der Heijden <rikvdh@users.noreply.github.com>
2025-04-24 09:26:41 +0200

    fix: post() and query() can also return array with a key given (fixes #2268) (#2270)
    * post and query can also be array with a key given (fixes #2268)
    
    * extend test for InteractsWithInput
    
    * extend test for null-key to interactswithinput post/query

--- tests/Type/data/request-object.php
+++ tests/Type/data/request-object.php
@@ -13,10 +13,20 @@ function test(Request $request): void
     assertType('array<int, Illuminate\Http\UploadedFile>', $request->file());
     assertType('Illuminate\Http\UploadedFile|null', $request->file('foo'));
     assertType('array<int, Illuminate\Http\UploadedFile>|Illuminate\Http\UploadedFile|stdClass', $request->file('foo', new \stdClass()));
+
     assertType('Illuminate\Routing\Route|null', $request->route());
     assertType('(object|string|null)', $request->route('foo'));
     assertType('(object|string)', $request->route('foo', 'bar'));
+
     assertType('array<string, string>', $request->server());
     assertType('string|null', $request->server('foo'));
     assertType('int|string', $request->server('foo', 5));
+
+    assertType('array', $request->post());
+    assertType('array|string|null', $request->post('foo'));
+    assertType('array|string', $request->post('foo', 'bar'));
+
+    assertType('array', $request->query());
+    assertType('array|string|null', $request->query('foo'));
+    assertType('array|string', $request->query('foo', 'bar'));
 }
