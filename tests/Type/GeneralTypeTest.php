416e421d3e7a6952e6583db17a6869a134ec2765
Liam Duckett <liamduckett2@gmail.com>
2025-12-01 18:54:55 +0000

    Stub has events for better inference on dispatches events property & method

--- tests/Type/GeneralTypeTest.php
+++ tests/Type/GeneralTypeTest.php
@@ -39,6 +39,7 @@ class GeneralTypeTest extends TypeInferenceTestCase
         yield from self::gatherAssertTypes(__DIR__ . '/data/facades.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/form-request.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/gate-facade.php');
+        yield from self::gatherAssertTypes(__DIR__ . '/data/has-events.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/helpers.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/higher-order-collection-proxy-methods.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/model-factories.php');
