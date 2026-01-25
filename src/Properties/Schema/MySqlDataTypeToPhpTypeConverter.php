5b274f9738ee596b3b1565fff459ab59a0e86189
Yuta Nagamiya <y.nagamiya@gmail.com>
2026-01-25 21:04:01 +0900

    Improve type inference for MySQL `ENUM` types in `MySqlDataTypeToPhpTypeConverter`

--- src/Properties/Schema/MySqlDataTypeToPhpTypeConverter.php
+++ src/Properties/Schema/MySqlDataTypeToPhpTypeConverter.php
@@ -4,18 +4,32 @@ declare(strict_types=1);
 
 namespace Larastan\Larastan\Properties\Schema;
 
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\VerbosityLevel;
+
+use function array_map;
 use function in_array;
 
 final class MySqlDataTypeToPhpTypeConverter
 {
-    /** @param list<lowercase-string> $options */
-    public function convert(string $type, array $options): string
+    /**
+     * @param list<lowercase-string> $options
+     * @param list<string>           $values
+     */
+    public function convert(string $type, array $options, array $values = []): string
     {
         return match ($type) {
             'CHAR', 'VARCHAR', 'TINYTEXT', 'TEXT', 'MEDIUMTEXT', 'LONGTEXT', 'BINARY', 'VARBINARY', 'DATE', 'DATETIME', 'TIMESTAMP', 'TIME', 'TINYBLOB', 'BLOB', 'MEDIUMBLOB', 'JSON' => 'string',
             'BIT', 'TINYINT', 'SMALLINT', 'MEDIUMINT', 'INT', 'INTEGER', 'BIGINT', 'YEAR' => in_array('unsigned', $options, true) ? 'non-negative-int' : 'int',
             'DECIMAL', 'DEC', 'NUMERIC', 'FIXED', 'FLOAT', 'DOUBLE', 'DOUBLE PRECISION', 'REAL' => 'float',
             'BOOL', 'BOOLEAN' => 'bool',
+            'ENUM' => (static function () use ($values): string {
+                return TypeCombinator::union(...array_map(
+                    static fn (string $value): ConstantStringType => new ConstantStringType($value),
+                    $values,
+                ))->describe(VerbosityLevel::value());
+            })(),
             default => 'mixed',
         };
     }
