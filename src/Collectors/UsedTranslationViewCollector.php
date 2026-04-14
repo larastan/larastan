254df868dd69d8c5789aee2856cf3ac6e9f168f0
Felix Bernhard <mail@felix-bernhard.com>
2026-04-14 20:37:48 +0200

    make regex case-insensitive

--- src/Collectors/UsedTranslationViewCollector.php
+++ src/Collectors/UsedTranslationViewCollector.php
@@ -23,7 +23,7 @@ use const PREG_SET_ORDER;
 final class UsedTranslationViewCollector
 {
     /** @see https://regex101.com/r/akPfPj/1 */
-    private const TRANSLATION_REGEX = '/(?<!\w)(trans|trans_choice|Lang::get|Lang::choice|Lang::trans|Lang::transChoice|@lang|@choice|__|\$t)\((?P<quote>[\'"])(?P<string>(?:\\k{quote}|(?!\k{quote}).)*?)\k{quote}[\),]/m';
+    private const TRANSLATION_REGEX = '/(?<!\w)(trans|trans_choice|Lang::get|Lang::choice|Lang::trans|Lang::transChoice|@lang|@choice|__|\$t)\((?P<quote>[\'"])(?P<string>(?:\\k{quote}|(?!\k{quote}).)*?)\k{quote}[\),]/im';
 
     public function __construct(private ViewParser $viewParser, private ViewFileHelper $viewFileHelper)
     {
