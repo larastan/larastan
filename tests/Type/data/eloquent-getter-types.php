75a7809fdf83096752408d5c614c2818a248ffa6
Denys Finchenko <dfinchenko@gmail.com>
2026-06-17 17:12:06 +0200

    feat: narrow getAppends and getMutatedAttributes return types

--- tests/Type/data/eloquent-getter-types.php
+++ tests/Type/data/eloquent-getter-types.php
@@ -10,4 +10,6 @@ function test(User $user): void
 {
     assertType('array<string, string>', $user->getCasts());
     assertType('list<string>', $user->getTouchedRelations());
+    assertType('list<string>', $user->getAppends());
+    assertType('list<string>', $user->getMutatedAttributes());
 }
