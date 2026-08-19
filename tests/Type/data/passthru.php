ad72c97e82f942e49339d835fee6beff10f90c92
Alexander Kartavenko <askemailbox@gmail.com>
2026-08-19 14:59:33 +0300

    fix: sync BuilderHelper::$passthru with Eloquent Builder's $passthru

--- tests/Type/data/passthru.php
+++ tests/Type/data/passthru.php
@@ -8,4 +8,13 @@ use function PHPStan\Testing\assertType;
 
 function test() {
     assertType('Illuminate\Database\Eloquent\Builder<App\User>', User::getCountForPagination());
+    assertType('mixed', User::query()->aggregate('sum', ['id']));
+    assertType('mixed', User::query()->doesntExistOr(fn () => 'x'));
+    assertType('mixed', User::query()->existsOr(fn () => 'x'));
+    assertType('Illuminate\Support\Collection', User::query()->explain());
+    assertType('array{select: list<mixed>, from: list<mixed>, join: list<mixed>, where: list<mixed>, groupBy: list<mixed>, having: list<mixed>, order: list<mixed>, union: list<mixed>, unionOrder: list<mixed>}', User::query()->getRawBindings());
+    assertType('string', User::query()->implode('name'));
+    assertType('int', User::query()->insertOrIgnoreUsing(['name'], 'select 1'));
+    assertType('float|int', User::query()->numericAggregate('sum', ['id']));
+    assertType('mixed', User::query()->rawValue('max(id)'));
 }
