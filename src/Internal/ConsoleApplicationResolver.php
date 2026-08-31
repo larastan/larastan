7080978a26300312f17554a8eca15fd8d4f5108d
Can Vural <can9119@gmail.com>
2026-09-01 01:15:35 +0200

    perf: memoize command lookups in ConsoleApplicationResolver

--- src/Internal/ConsoleApplicationResolver.php
+++ src/Internal/ConsoleApplicationResolver.php
@@ -9,37 +9,43 @@ use Illuminate\Console\Command;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher;
 use PHPStan\Reflection\ClassReflection;
-use PHPStan\Type\ObjectType;
 
 use function app;
+use function array_key_exists;
+use function is_a;
 
 /** @internal */
 final class ConsoleApplicationResolver
 {
     private Application|null $application = null;
 
+    /** @var array<string, Command[]> */
+    private array $commandsByClass = [];
+
     /** @return Command[] */
     public function findCommands(ClassReflection $classReflection): array
     {
-        $consoleApplication = $this->getApplication();
+        $className = $classReflection->getName();
 
-        $classType = new ObjectType($classReflection->getName());
+        if (array_key_exists($className, $this->commandsByClass)) {
+            return $this->commandsByClass[$className];
+        }
 
-        if (! (new ObjectType('Illuminate\Console\Command'))->isSuperTypeOf($classType)->yes()) {
-            return [];
+        if (! $classReflection->is(Command::class)) {
+            return $this->commandsByClass[$className] = [];
         }
 
         $commands = [];
 
-        foreach ($consoleApplication->all() as $name => $command) {
-            if (! $classType->isSuperTypeOf(new ObjectType($command::class))->yes()) {
+        foreach ($this->getApplication()->all() as $name => $command) {
+            if (! is_a($command, $className)) {
                 continue;
             }
 
             $commands[$name] = $command;
         }
 
-        return $commands; // @phpstan-ignore-line
+        return $this->commandsByClass[$className] = $commands; // @phpstan-ignore-line
     }
 
     private function getApplication(): Application
